9325e67836e4225cadae23fd51fbc7c536881baf
Sasha <Sasha@ACER>
2021-10-22 08:47:06 +0300

    Добавьте файлы проекта.

---
+++ module1_lab3/task5.cpp
@@ -0,0 +1,59 @@
+﻿// module1_lab3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+double fun(double x) {  //обчислює значення функції
+    int p;
+    p = pow(x, 3) + x - 5;
+    return p;
+}
+double fun_p1(double x) { //обчислює значення першої похідної функції
+    int p;
+    p = 3 * pow(x, 2) + 1;
+    return p;
+}
+double fun_p2(double x) { //обчислює значення другої рохідної функції
+    int p;
+    p = 6 * x;
+    return p;
+}
+int main()
+{
+    double a, b, e, x0, x1;
+    cout << "a=";
+    cin >> a;
+    cout << "b=";
+    cin >> b;
+    cout << "e=";
+    cin >> e;
+    bool k = true;
+
+    if (fun(a) * fun(b) > 0) { //перевірка умов методу
+        k = false;
+    }
+    if (k) {
+        if (fun(a) * fun_p2(a) > 0) {
+            x0 = a;
+            x1 = a - fun(a) / fun_p1(a);
+        }
+        else {
+            if (fun(b) * fun_p2(b) > 0) {
+                x0 = b;
+                x1 = b - fun(b) / fun_p1(b);
+            }
+            else k = false;
+        }
+
+    }
+
+    if (k) {
+        while (abs(x1 - x0) >= e) {//знаходження послідовності х,що збігаються до кореня
+            x0 = x1;
+            x1 = x1 - fun(x1) / fun_p1(x1);
+        }
+        cout << "koren: " << x1;
+    }
+    else cout << "ymova dla metody ne vykonuetsya";
+}
+
---
+++ task15/task15.cpp
@@ -0,0 +1,42 @@
+﻿// task15.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+double fun_lag(double nn, double* XX, double* YY, double xk) { //побудова многочлена Лагранжа
+    double yk = 0;
+    for (int i = 0; i < nn; i++) {
+        double p = 1;
+        for (int j = 0; j < nn; j++) {
+            if (j != i) {
+                p = p * (xk - XX[j]) / (XX[i] - XX[j]);
+            }
+
+        }
+        yk = yk + p * YY[i];
+    }
+    return yk;
+}
+int main()
+{
+    int n;
+    double x, y;
+    cout << "vvesty kilkist vusliv: ";
+    cin >> n;
+
+    double* X = new double[n]; //масив абсцис вузлів інтерполяції
+    double* Y = new double[n]; //масив ординат вузлів інтерполяції
+    cout << "vvesty koordynaty vusliv: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "x" << i + 1 << ": ";
+        cin >> X[i];
+        cout << "y" << i + 1 << ": ";
+        cin >> Y[i];
+    }
+    cout << "x: ";
+    cin >> x;
+    y = fun_lag(n, X, Y, x); //обчислення значення многочлена лагранжа для проміжку х
+    cout << "y: " << y;
+
+}
+
